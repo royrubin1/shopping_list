09ac9ad8604a24cd5d0553e13388f1b94370c2d7
royrubin1 <royrubin@protonmail.com>
2023-06-29 23:44:51 +0200

    Class203

---
+++ lib/models/category.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other
+}
+
+class Category {
+  const Category(this.title, this.color);
+  final String title;
+  final Color color;
+}
+
+/*
+class Category extends StatelessWidget {
+  const Category({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}*/
